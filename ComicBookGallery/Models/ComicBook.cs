babb0ad09320a02f4beee5260854bb4e32a42c51
AndreiDogaru <andrei_dogaru12@yahoo.com>
2017-07-22 15:51:42 +0200

    added comic book and artist models

---
+++ ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string Description { get; set; }
+        public string[] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+        public ComicBook()
+        {
+        }
+    }
+}
